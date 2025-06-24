fabb281a386fb8a92b714512fefd35edc58e3304
Nwosu chinenyenwa <nwosuchinenyenwa4@gmail.com>
2025-06-24 10:33:23 +0100

    fixed

--- src/component/Loader/Loader.jsx
+++ src/component/Loader/Loader.jsx
@@ -1,38 +1,45 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Loader.css';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Loader.css";
 
 export default function Loader() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const hasSeenLoader = sessionStorage.getItem('hasSeenLoader');
+    const hasSeenLoader = sessionStorage.getItem("hasSeenLoader");
 
     if (hasSeenLoader) {
-      navigate('/form');
+      navigate("/form");
     } else {
       const timer = setTimeout(() => {
-        sessionStorage.setItem('hasSeenLoader', 'true');
-        navigate('/form');
-      }, 2000);
+        sessionStorage.setItem("hasSeenLoader", "true");
+        navigate("/form");
+      }, 5000);
 
       return () => clearTimeout(timer);
     }
   }, [navigate]);
 
-
-  const hasSeenLoader = sessionStorage.getItem('hasSeenLoader');
+  const hasSeenLoader = sessionStorage.getItem("hasSeenLoader");
   if (hasSeenLoader) return null;
 
   return (
     <section className="load">
-      <div className="three-body">
-        <div className="three-body__dot"></div>
-        <div className="three-body__dot"></div>
-        <div className="three-body__dot"></div>
-      </div>
-      <div className="loader">
-        <span></span>
+      <div id="wifi-loader">
+        <svg class="circle-outer" viewBox="0 0 86 86">
+          <circle class="back" cx="43" cy="43" r="40"></circle>
+          <circle class="front" cx="43" cy="43" r="40"></circle>
+          <circle class="new" cx="43" cy="43" r="40"></circle>
+        </svg>
+        <svg class="circle-middle" viewBox="0 0 60 60">
+          <circle class="back" cx="30" cy="30" r="27"></circle>
+          <circle class="front" cx="30" cy="30" r="27"></circle>
+        </svg>
+        <svg class="circle-inner" viewBox="0 0 34 34">
+          <circle class="back" cx="17" cy="17" r="14"></circle>
+          <circle class="front" cx="17" cy="17" r="14"></circle>
+        </svg>
+        <div class="text" data-text="Loading"></div>
       </div>
     </section>
   );
