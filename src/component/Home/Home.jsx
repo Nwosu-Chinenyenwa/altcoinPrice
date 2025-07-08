3962b0b5b220119822a70d017bd5798b23b8b7a3
Nwosu chinenyenwa <nwosuchinenyenwa4@gmail.com>
2025-07-08 16:19:46 +0100

    finished with this version

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -17,7 +17,7 @@ export default function Home() {
   const { allCoins, currency } = useContext(CoinContext);
   const [displayCoin, setDisplayCoin] = useState([]);
   const [input, setInput] = useState("");
-  const [have, sethave] = useState(0);
+  const [have, sethave] = useState(1);
   const { setCurrency } = useContext(CoinContext);
   const [user, setUser] = useState(null);
 
@@ -221,6 +221,7 @@ export default function Home() {
             </div>
           </ul>
         </nav>
+        
         {use && (
           <div class="card-client">
             <div class="user-picture">
@@ -270,10 +271,14 @@ export default function Home() {
                 <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
               </g>
             </svg>
-            <input onChange={inputHandler} placeholder="Search" type="search" className="input" />
+            <input
+              onChange={inputHandler}
+              placeholder="Search"
+              type="search"
+              className="input"
+            />
           </div>
         </section>
-        
       </header>
       <main className="market-main">
         <div className="coins">
